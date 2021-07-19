2add78a8ebd963f30899e404623826b863547f02
Saqib Ishaq <saqib.ishaq@techloyce.com>
2021-07-19 18:45:25 +0500

    initi

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [name, setName] = useState('Click Me');
+  const changeHandler = () => {
+    setName('Boom!');
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <button onClick={changeHandler} >{name}</button>
     </div>
   );
 }
